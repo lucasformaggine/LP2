3d4685efd62a2a6a2c372e39aa1bd1e4a922a0a9
Lucas <lucasformaggine@hotmail.com>
2021-05-30 15:11:31 -0300

    Applied red selection as IVisible method

--- Projeto/interfaces/IVisible.java
+++ Projeto/interfaces/IVisible.java
@@ -5,4 +5,5 @@ import java.awt.*;
 public interface IVisible {
     public boolean IsInsideFigure(Point mousePointPosition);
     public void Paint(Graphics g);
+    public void applyRedSelection(Graphics g);
 }
